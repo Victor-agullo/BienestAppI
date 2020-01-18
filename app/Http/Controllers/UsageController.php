3a74131c023604da1f5b7b5053d28e0df8529dc0
Víctor <victor_jesus_agullo_apps1ma1819@cev.com>
2020-01-18 22:12:41 +0100

    Cambios en el json que se mandaba al pedir los tiempos de las apps, ahora todo es más fácil de sacar del json decodificado

--- app/Http/Controllers/UsageController.php
+++ app/Http/Controllers/UsageController.php
@@ -18,30 +18,29 @@ class UsageController extends Controller
 
     public function times()
     {
+        $pack=[];
         $id_user = $this->identify->idUserGetter();
 
         $array = Application::select('icon', 'name')
             ->get();
 
-        $rows = count($array);
-        for ($i = 0; $i < $rows; $i++) {
+        $rows = count($array) + 1;
+        for ($i = 1; $i < $rows; $i++) {
             $data = [
                 'id_user' => $id_user,
-                'id_app' => $rows
+                'id_app' => $i
             ];
 
+            $iconName = (array)json_decode($array[$i - 1]);
             $total = $this->use->totalTime($data);
             $daily = $this->use->dailyTime($data);
-
-            $pack[$i] = [
-                $array[$i],
-                $total,
-                $daily
-            ];
+            
+            $pack[$i] = array_merge($iconName, $total, $daily);
         }
 
-        return response()->json([
-            $pack
-        ], 200);
+        return response()->json(
+            $pack,
+            200
+        );
     }
 }
