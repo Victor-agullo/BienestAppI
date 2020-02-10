db71a9ab0b416a5b3ee27a754c50dadb4fc54a76
Víctor <38455769+Victor-agullo@users.noreply.github.com>
2020-02-10 23:54:51 +0100

    terminada función que devuelve las restricciones en sucio, pasar a limpio

--- app/Http/Controllers/RestrictController.php
+++ app/Http/Controllers/RestrictController.php
@@ -6,6 +6,7 @@ use App\Helpers\Identifier;
 use App\Restrict;
 use Illuminate\Http\Request;
 use Illuminate\Queue\Console\RetryCommand;
+use Illuminate\Support\Facades\DB;
 
 class RestrictController extends Controller
 {
@@ -29,12 +30,21 @@ class RestrictController extends Controller
             'id_user' => $id_user,
         ];
 
-        $restriction = Restrict::SELECT('applications.name')
+        $restriction = DB::select( DB::raw("Select applications.name
+        FROM restricts
+        join usages on restricts.id_app = usages.id_app AND restricts.`id_user`=usages.id_user
+        join applications on restricts.id_app = applications.id
+        WHERE time < restricts.max_time"));
+        
+        /*
+        DB::table('restricts')
         ->JOIN('usages','restricts.id_app','=','usages.id_app')
         ->JOIN('usages','restricts.id_user','=','usages.id_user')
-        ->JOIN('applications','ON','restricts.id_app','=','applications.id')
+        ->JOIN('applications','restricts.id_app','=','applications.id')
+        ->SELECT('applications.name')
         ->WHERE('time','>','restricts.max_time')
         ->GET();
+        */
 
         return $restriction;
     }
